f26aa27cfcba47adbaa999a14bec0659ae6e7ac1
BrakeDude <lockbrake@gmail.com>
2024-02-18 18:50:19 +0300

    Fixed DSS error

--- lua/deadseascrolls.lua
+++ lua/deadseascrolls.lua
@@ -294,7 +294,7 @@ local function OverridePause(self, player, hook, action)
 				end
 			end
 		end
-		return 0.75
+		return true
 	end
 end
 ComplianceImmortal:AddCallback(ModCallbacks.MC_INPUT_ACTION, OverridePause, InputHook.IS_ACTION_PRESSED)
